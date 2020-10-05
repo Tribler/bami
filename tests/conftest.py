cc8ab303cb86cf670a68fbf7b71b45f21860e528
Martijn de Vos <mdmartijndevos@gmail.com>
2020-10-05 14:26:33 +0200

    Removed unused mock_requests_get

--- tests/conftest.py
+++ tests/conftest.py
@@ -22,16 +22,6 @@ def pytest_configure(config: Config) -> None:
     config.addinivalue_line("markers", "e2e: mark as end-to-end test.")
 
 
-@pytest.fixture
-def mock_requests_get(mocker: MockFixture) -> Mock:
-    mock = mocker.patch("requests.get")
-    mock.return_value.__enter__.return_value.json.return_value = {
-        "title": "Lorem Ipsum",
-        "extract": "Lorem ipsum dolor sit amet",
-    }
-    return mock
-
-
 # Fixtures
 class FakeBlock(BamiBlock):
     """
